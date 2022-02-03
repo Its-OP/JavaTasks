ef248d74694541f76fa606176301362dd41bdc42
OPrl <prostakov.oleh@gmail.com>
2022-02-04 00:10:37 +0200

    Full task

--- Rectangles/IntersectionType.java
+++ Rectangles/IntersectionType.java
@@ -1,8 +1,10 @@
 public enum IntersectionType {
     NoIntersection("No Intersection"),
+    SinglePoint("Single Point"),
     HorizontalLine("Horizontal Line"),
+    VerticalLine("Vertical Line"),
+    Rectangle("Rectangle Type"),
     SomethingElse("Something Else");
-
     private String description;
 
     IntersectionType(String intersectionDescription){
--- Rectangles/Program.java
+++ Rectangles/Program.java
@@ -2,11 +2,12 @@ class Program{
     public static void main(String[] args) {
         //Test samples
         Rectangle firstRec = new Rectangle(1, 4, 6, 7);
-        Rectangle secondRec = new Rectangle(2, 5, 7, 8);
+        Rectangle secondRec = new Rectangle(4, 7, 7, 8);
         System.out.println(FindIntersection(firstRec, secondRec).getDescription());
     }
 
     public static IntersectionType FindIntersection(Rectangle firstRec, Rectangle secondRec){
+        //#region Necessary Task
         if(
 
             //Only when two opposite (bottom and top) horizontal sides are located on the same Y-level and overlap,
@@ -15,9 +16,11 @@ class Program{
             //If the opposite (bottom and top) horizontal sides of two rectangles are located on the same Y-level
             (firstRec.bottomY == secondRec.topY || firstRec.topY == secondRec.bottomY) &&
                 //And at least one point of a rectangle is located between two points of another rectangle
-                ((firstRec.rightX > secondRec.rightX && firstRec.leftX < secondRec.rightX) ||
-                (firstRec.rightX > secondRec.leftX && firstRec.leftX < secondRec.leftX))
+                ((firstRec.rightX >= secondRec.rightX && firstRec.leftX < secondRec.rightX) ||
+                (firstRec.rightX > secondRec.leftX && firstRec.leftX <= secondRec.leftX) ||
+                (firstRec.rightX <= secondRec.rightX && firstRec.leftX >= secondRec.leftX))
         ){return IntersectionType.HorizontalLine;}
+
         else if(
 
             //Only when any of the next conditions is met, we can state that the rectangles do not intersect
@@ -31,8 +34,37 @@ class Program{
             //If the bottom side of the first rectangle is located higher than the top side of the second rectangle
             firstRec.bottomY > secondRec.topY
         ){return IntersectionType.NoIntersection;}
+        //#endregion
+
+        //#region Full Task
+        else if(
+
+            //Only when two sides of one rectangle match two opposite sides of other rectangle
+            //(e.g. bottom and right of the first one match top and left of the second one)
+            //the intersection is one point.
+
+            //If horizontal side of one rectangle match an opposite horizontal side of the second rectangle
+            (firstRec.rightX == secondRec.leftX || firstRec.leftX == secondRec.rightX) &&
+            //And vertical side of one rectangle match an opposite vertical side of the second rectangle
+            (firstRec.topY == secondRec.bottomY || firstRec.bottomY == secondRec.topY)
+        ){return IntersectionType.SinglePoint;}
+
+        else if(
+
+            //Vertical side intersection task's logic works likewise once of horizontal side
+
+            (firstRec.leftX == secondRec.rightX || firstRec.rightX == secondRec.leftX) &&
+                ((firstRec.topY > secondRec.topY && firstRec.bottomY < secondRec.topY) ||
+                (firstRec.topY > secondRec.bottomY && firstRec.bottomY < secondRec.topY) ||
+                (firstRec.topY < secondRec.topY && firstRec.bottomY > secondRec.bottomY))
+        ){return IntersectionType.VerticalLine;}
+
         else{
-            return IntersectionType.SomethingElse;
+
+            //There are 5 possible intersection types - nothing, a single point, a horizontal line, a vertical line and 
+            //a rectangle. If an intersection type does not match any of ones, I've found before, it must be a rectangle.
+
+            return IntersectionType.Rectangle;
         }
     }
 }
